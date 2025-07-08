4b236cf7b02bd86cbb10af83dbbc801b67edae02
venkat54638 <venkatsai.anilkumar@minfytech.com>
2025-07-08 14:35:26 +0530

    Update train.py

--- train.py
+++ train.py
@@ -24,12 +24,10 @@ y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-# Save report
-with open("cml_report.md", "w") as f:
-    f.write("# California Housing Model Report\n\n")
-    f.write(f"- **MSE**: {mse:.4f}\n")
-    f.write(f"- **R² Score**: {r2:.4f}\n\n")
-    f.write(f"![Prediction Plot](scatter.png)\n")
+
+print(f"- **MSE**: {mse:.4f}\n")
+print(f"- **R² Score**: {r2:.4f}\n\n")
+print(f"![Prediction Plot](scatter.png)\n")
 
 # Plot
 plt.figure(figsize=(8, 6))
